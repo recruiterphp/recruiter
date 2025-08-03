1610eb376fff5edce50dd4733a2afe4c5cab743b
D. Bellettini <325358+dbellettini@users.noreply.github.com>
2025-08-03 12:22:29 +0200

    Add PHP-CS-Fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+return new PhpCsFixer\Config()
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        '@Symfony' => true,
+        'array_indentation' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'concat_space' => ['spacing' => 'one'],
+        'declare_strict_types' => true,
+        'string_implicit_backslashes' => true,
+        'list_syntax' => ['syntax' => 'short'],
+        'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
+        'ordered_imports' => true,
+        'phpdoc_to_comment' => false,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays', 'arguments', 'parameters']],
+        'visibility_required' => ['elements' => ['property', 'method', 'const']],
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__ . '/src')
+            ->in(__DIR__ . '/spec')
+    )
+;
