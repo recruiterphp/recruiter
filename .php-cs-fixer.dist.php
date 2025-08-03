26e2aeca1292b5a7e79cc1e159a38481b3b480a2
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2025-08-03 12:23:06 +0200

    Disable strict types for now

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,7 +8,7 @@ return new PhpCsFixer\Config()
         'array_indentation' => true,
         'array_syntax' => ['syntax' => 'short'],
         'concat_space' => ['spacing' => 'one'],
-        'declare_strict_types' => true,
+        /* 'declare_strict_types' => true, */
         'string_implicit_backslashes' => true,
         'list_syntax' => ['syntax' => 'short'],
         'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
